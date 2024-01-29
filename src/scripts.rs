1bb80af388e5cdf13e892664d4ee7816b77a4deb
Mark Cha <markcha723@gmail.com>
2024-01-29 10:15:36 -0800

    day 2 part 1

--- src/scripts.rs
+++ src/scripts.rs
@@ -1,6 +1,7 @@
 pub mod nineteen;
-use nineteen::day_one;
+use nineteen::{day_one, day_two};
 
 pub fn run_scripts() {
     day_one();
+    day_two();
 }
--- src/scripts/nineteen.rs
+++ src/scripts/nineteen.rs
@@ -3,7 +3,7 @@ use std::io::prelude::*;
 use std::path::Path;
 
 pub fn day_one() {
-    let path = Path::new("src/scripts/inputs/nineteen.txt");
+    let path = Path::new("src/scripts/inputs/one.txt");
 
     let mut file = match File::open(&path) {
         Ok(file) => file,
@@ -35,4 +35,67 @@ pub fn day_one() {
         };
     }
     println!("{}", sum.to_string());
+}
+
+pub fn day_two() {
+    let path: &Path = Path::new("src/scripts/inputs/two.txt");
+
+    let mut file: File = match File::open(&path) {
+        Ok(file) => file,
+        Err(why) => panic!("couldn't open the file: {}", why),
+    };
+
+    let mut s: String = String::new();
+
+    let s: String = match file.read_to_string(&mut s) {
+        Ok(_) => s,
+        Err(why) => panic!("couldn't read to string: {}", why),
+    };
+
+    fn str_to_vec(s: &str) -> Result<Vec<i32>, std::num::ParseIntError> {
+        s.split(",").map(|n| n.trim().parse()).collect()
+    }
+
+    let mut s: Vec<i32> = match str_to_vec(&s) {
+        Ok(vec) => vec,
+        Err(why) => panic!("error parsing string into vec of ints: {}", why),
+    };
+
+    fn run_operations(mut codes: Vec<i32>) -> Vec<i32> {
+        let mut code_start_index: usize = 0;
+        loop {
+            let code_slice: &[i32] = &codes[code_start_index..(code_start_index + 4)];
+
+            match code_slice[0] {
+                1 => {
+                    let input_one_index = code_slice[1] as usize;
+                    let input_two_index = code_slice[2] as usize;
+                    let save_index = code_slice[3] as usize;
+
+                    let sum: i32 = codes[input_one_index] + codes[input_two_index];
+                    codes[save_index] = sum;
+                    code_start_index += 4;
+                }
+                2 => {
+                    let input_one_index = code_slice[1] as usize;
+                    let input_two_index = code_slice[2] as usize;
+                    let save_index = code_slice[3] as usize;
+
+                    let product: i32 = codes[input_one_index] * codes[input_two_index];
+                    codes[save_index] = product;
+                    code_start_index += 4;
+                }
+                99 => {
+                    println!("99!");
+                    break;
+                }
+                _ => panic!("code started with an invalid number!"),
+            }
+        }
+        codes
+    }
+
+    s[1] = 12;
+    s[2] = 2;
+    println!("{}", run_operations(s)[0]);
 }
